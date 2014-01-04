68be34a06ac88f6d454e48d908cfe08771a2df70
Wilson Wan <wanzheng@gmail.com>
2014-01-04 18:18:33 +0800

    initial commit

---
+++ WechatReader/RDRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RDRAppDelegate.h
+//  WechatReader
+//
+//  Created by WanZheng on 4/1/14.
+//  Copyright (c) 2014 cos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RDRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WechatReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WechatReader
+//
+//  Created by WanZheng on 4/1/14.
+//  Copyright (c) 2014 cos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RDRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RDRAppDelegate class]));
+    }
+}
