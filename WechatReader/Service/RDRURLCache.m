81c5e63ca34ea963a61edc1e14d1137b23e6a2f0
Wilson Wan <wanzheng@gmail.com>
2014-01-07 18:44:31 +0800

    测试自建cache

--- WechatReader/Service/RDRURLCache.m
+++ WechatReader/Service/RDRURLCache.m
@@ -22,6 +22,16 @@
 }
 
 - (NSCachedURLResponse *)cachedResponseForRequest:(NSURLRequest *)request {
+    if ([request.URL.absoluteString isEqualToString:@"http://mp.weixin.qq.com/mp/appmsg/show?__biz=MjM5Njg4NzY2MQ==&appmsgid=10013662&itemidx=2&sign=e02e06baf2534a24eb383faeae3add44#wechat_redirect"]) {
+        NSData *data = [@"<html>Hello</html>" dataUsingEncoding:NSUTF8StringEncoding];
+        NSURLResponse *urlResponse = [[NSURLResponse alloc] initWithURL:request.URL
+                                                               MIMEType:@"text/html"
+                                                  expectedContentLength:data.length
+                                                       textEncodingName:nil];
+        NSCachedURLResponse *cachedURLResponse = [[NSCachedURLResponse alloc] initWithResponse:urlResponse data:data];
+        return cachedURLResponse;
+    }
+
     NSCachedURLResponse *response = [self.oldSharedCache cachedResponseForRequest:request];
     NSLog(@"cache response for request(%@) = %@", request.URL.absoluteString, response);
     return response;
