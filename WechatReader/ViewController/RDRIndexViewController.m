cd9e673757897483ff1d959ccacea75506aaeb8b
WilsonWan <wanzheng@gmail.com>
2014-01-08 12:56:51 +0800

    fix compile warning

--- WechatReader/ViewController/RDRIndexViewController.m
+++ WechatReader/ViewController/RDRIndexViewController.m
@@ -62,7 +62,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    id <NSFetchedResultsSectionInfo> sectionInfo = [[self.fetchedResultsController sections] objectAtIndex:section];
+    id <NSFetchedResultsSectionInfo> sectionInfo = [[self.fetchedResultsController sections] objectAtIndex:(NSUInteger)section];
     return [sectionInfo numberOfObjects];
 }
 
